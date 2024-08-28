05dfd6d53a80f7b8e9ef4695d42ff07b8ea450f2
Rakesh03-cmd <rakeshtummla@gmail.com>
2024-08-28 15:43:26 +0530

    addition

---
+++ add.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Ask user for first number
+echo "Enter first number:12 "
+
+# Ask user for second number
+echo "Enter second number:12 "
+
+# Perform addition
+sum=$((12 + 12))
+
+# Display result
+echo "$sum"
